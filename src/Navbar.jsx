1e376ee103f1065eeb49f1c36f777c58066d0039
w1z13 <115799235+w1z13@users.noreply.github.com>
2023-11-08 10:22:13 +0100

    changed navbar title to fake shop

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar({cart}) {
   return (
     <nav>
       <div className="left">
-        <h1>Real Shop</h1>
+        <h1>Fake Shop</h1>
         <NavLink to="/" >Home</NavLink>
         <NavLink to="items">Items</NavLink>
       </div>
